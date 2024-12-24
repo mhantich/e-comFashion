ad8ca214f1e8e94ba98840f7ed07d2d2bcc4609c
mhantich <mohame.mhantich6@gmail.com>
2024-12-24 17:27:00 +0000

    Add tailwindcss-animate plugin to Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from 'tailwindcss-animate';
+
 export default {
   darkMode: ['class'],
   content: [
@@ -80,4 +82,5 @@ export default {
       },
     },
   },
+  plugins: [tailwindcssAnimate],
 };
